cdb6dfa443861eea51311411fe9aaa4b9f619008
Slimane Lakehal <lakehalslimane@gmail.com>
2026-05-25 00:55:46 +0200

    terraform ressources normalisation (stripping out of main)

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "gcs" {
+    bucket = "lumio-terraform-state"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -10,21 +15,4 @@ terraform {
 provider "google" {
   project = var.project
   region  = var.region
-}
-
-# GCS bucket for raw data
-resource "google_storage_bucket" "raw_data" {
-  name     = "lumio-raw-data-dev"
-  location = var.region
-}
-
-# BigQuery datasets
-resource "google_bigquery_dataset" "raw_cegid" {
-  dataset_id = "raw_cegid"
-  location   = var.region
-}
-
-resource "google_bigquery_dataset" "stg_cegid" {
-  dataset_id = "stg_cegid"
-  location   = var.region
 }
